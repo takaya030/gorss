266ca95898ac77f9988f43140cd435bd32e55150
Minoru Takayama <takaya030@yahoo.co.jp>
2024-09-15 15:43:28 +0900

    modify main

--- main.go
+++ main.go
@@ -2,26 +2,17 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/takaya030/gorss/controller"
 )
 
 func main() {
-	engine := gin.Default()
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Printf("Cannot load env file: %v", err)
 	}
-	engine.GET("/", func(c *gin.Context) {
 
-		msg := os.Getenv("SAMPLE_MESSAGE")
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": msg,
-		})
-	})
-	engine.Run(":8080")
+	router := controller.GetRouter()
+	router.Run(":8080")
 }
