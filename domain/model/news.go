6f689ca4524665768bf5844c1c38b9c759d361c8
Minoru Takayama <takaya030@yahoo.co.jp>
2024-09-28 22:43:47 +0900

    modify news model

--- domain/model/news.go
+++ domain/model/news.go
@@ -1,7 +1,8 @@
 package model
 
 type News struct {
-	Id        string `json:"id"`
+	Id        string `json:"id,omitempty"`
+	Title     string `json:"title,omitempty"`
 	TimeStamp int64  `json:"timestamp"`
 	Url       string `json:"url"`
 }
